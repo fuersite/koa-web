27acb977706d65632e7883b610680fb3f2bfd9e4
ivin.zhou <1300243672@qq.com>
2019-08-22 09:57:23 +0800

    add session store”

--- server/app.js
+++ server/app.js
@@ -2,31 +2,35 @@ const Koa = require('koa')
 const path = require('path')
 const router = require('./routers/index')
 const bodyParser = require('koa-bodyparser')
-const loggerMiddleware = require('./middleware/logger.js')
 const koaLogger = require('koa-logger')
 const kaoStatic = require('koa-static') // 静态文件访问
 const session = require('koa-session-minimal') // session
 const views = require('koa-views') // 视图操作
-// const mongoose = require('mongoose')
-// mongoose.connect('mongodb://admin:123456@localhost:27017/user?authSource=admin')
-// const Cat = mongoose.model('animals', { name: String },'animal')
+const sessionStore = require('./utils/sessionStore') // 视图操作
+const mongoose = require('mongoose')
 const app = new Koa()
 
-// 使用session中间件
+// 使用session存cookie设置
 let cookie = {
   domain: 'localhost',  // 写cookie所在的域名
-  path: '/',       // 写cookie所在的路径
-  maxAge: 100*1000, // cookie有效时长
-  expires: new Date('2019-08-20'),  // cookie失效时间
-  httpOnly: true,  // 是否只用于http请求中获取
-  overwrite: true,  // 是否允许重写
-  store: true,
-  sameSite: '',
-  signed: '',
+  path: '/',            // 写cookie所在的路径
+  maxAge: 86400000,     // cookie有效时长
+  httpOnly: true,       // 是否只用于http请求中获取
+  overwrite: true,      // 是否允许重写
+  signed: true,
 }
 
+// 使用session存mongondb
+mongoose.connect('mongodb://admin:123456@localhost:27017/user?authSource=admin')
+let store =  new sessionStore({
+  collection: 'sessions',   //数据库集合
+  connection: mongoose,     // 数据库链接实例
+  expires: 86400,           // 默认时间为1天
+  name: 'session'           // 保存session的表名称
+})
+
 // 开启cookie-session
-app.use(session({key: 'SESSION_ID', cookie: cookie}))
+app.use(session({key: 'SESSION_ID', cookie, store}))
 
 // 配置控制台日志中间件
 app.use(convert(koaLogger()))
--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,8 @@
 class User {
-    static collectionName = 'users'
-    static fileds = ['id', 'name', 'password', 'email', 'nick', 'createTime', 'modifiedTime', 'level']
+    // 表名
+    collectionName = 'user'
+    // 对象属性名
+    fileds = ['_id', 'userName', 'password', 'email', 'nick', 'createdAt', 'modifiedAt', 'level']
+    // 表字段名
+    schemas = ['_id', 'user_name', 'real_name', 'password', 'email', 'nick', 'created_at', 'modified_at', 'level']
 }
