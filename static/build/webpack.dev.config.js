98e907187364f52d224378d2f25dbaec2ce173e0
fuersite <1300243672@qq.com>
2019-08-27 17:20:54 +0800

    fix koa-mongodb-webpack

--- static/build/webpack.dev.config.js
+++ static/build/webpack.dev.config.js
@@ -22,6 +22,37 @@ module.exports = merge(config, {
             collapseWhitespace:true,     //去掉文件的空白行，这里是把文件打包成一行
         },
         hash:true,
+        chunks: ['index', 'vendor']
+    }),
+    new HtmlWebpackPlugin({            //在这里实例化
+        template:path.join(__dirname,"..", "src", "index.html"),   //这里告诉他模板html放在什么位置
+        filename:'admin.html',         //这里是输出后的文件名，这里我还是叫index.html，可改
+        minify:{                       // 这里放缩小的条件
+            removeAttributeQuotes:true,  //去掉文件里的双引号
+            collapseWhitespace:true,     //去掉文件的空白行，这里是把文件打包成一行
+        },
+        hash:true,
+        chunks: ['admin', 'vendor']
+    }),
+    new HtmlWebpackPlugin({            //在这里实例化
+        template:path.join(__dirname,"..", "src", "index.html"),   //这里告诉他模板html放在什么位置
+        filename:'work.html',         //这里是输出后的文件名，这里我还是叫index.html，可改
+        minify:{                       // 这里放缩小的条件
+            removeAttributeQuotes:true,  //去掉文件里的双引号
+            collapseWhitespace:true,     //去掉文件的空白行，这里是把文件打包成一行
+        },
+        hash:true,
+        chunks: ['work', 'vendor']
+    }),
+    new HtmlWebpackPlugin({            //在这里实例化
+        template:path.join(__dirname,"..", "src", "index.html"),   //这里告诉他模板html放在什么位置
+        filename:'error.html',         //这里是输出后的文件名，这里我还是叫index.html，可改
+        minify:{                       // 这里放缩小的条件
+            removeAttributeQuotes:true,  //去掉文件里的双引号
+            collapseWhitespace:true,     //去掉文件的空白行，这里是把文件打包成一行
+        },
+        hash:true,
+        chunks: ['error', 'vendor']
     })
 ]
 });
